9ae8b8051e4cf5f3b5425d827b25736640547be2
Coal Cooper <cocowmn@gmail.com>
2024-07-17 02:39:46 -0400

    Initial Commit

---
+++ Tip 'n Split/Tip__n_SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip__n_SplitApp.swift
+//  Tip 'n Split
+//
+//  Created by Coal Cooper on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Tip__n_SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
